f399d86bc64f7ccbb4d321ba4eb0bc33ddc44e6d
Walras <el.cumitini@proton.me>
2024-09-11 12:33:04 +0200

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
+import { GreencantoLanding } from "@/components/split.greencanto-landing"
 import { GreencantoLandingFull } from "@/components/greencanto-landing"
-
 
 export default function Home() {
   return <GreencantoLandingFull />
